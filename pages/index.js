5f1f86e9917371e492d1e891cd1d5d96224b2d4a
Zuzana Kůtová <zuzana.kutova@seznam.cz>
2019-06-28 11:08:52 +0200

    added primer

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,27 @@
 import Hello from '../components/Hello'
+import {
+    Box,
+    Button,
+    Heading,
+    Text,
+    BaseStyles,
+} from '@primer/components'
 
 
 function Home() {
     return <div>
-        <Hello name='Zuz'></Hello>
-        <p>
-        <a href="/aboutme">Im Zuz </a>
-        </p>
+        <BaseStyles>
+            <Box m={4}>
+                <Heading mb={2}>Hello, Zuz!</Heading>
+                <p>This will get Primer text styles.</p>
+
+                <Hello name='Zuz'></Hello>
+                <p>
+                    <a href="/aboutme">Im Zuz </a>
+                </p>
+            </Box>
+        </BaseStyles>
+
     </div>;
 }
 
