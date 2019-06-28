481e6e6b06af4f45af93429cebc171ca337fdd39
Zuzana Kůtová <zuzana.kutova@seznam.cz>
2019-06-28 10:50:23 +0200

    new components

---
+++ components/Hello.js
@@ -0,0 +1,10 @@
+let Hello = (props) => {
+    return <>
+        <h1 style={{
+            color: 'pink',
+            backgroundColor: 'black',
+        }}> Hello {props.name}</h1>
+    </>
+}
+
+export default Hello;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
+import Hello from '../components/Hello'
+
+
 function Home() {
     return <div>
-        Welcome to Vnitroblock
+        <Hello name='Zuz'></Hello>
         <p>
         <a href="/aboutme">Im Zuz </a>
         </p>
